10be04b40eba425d2cec7c849827b8665f32b904
AhmedRahi18 <71ahmedrahi@gmail.com>
2023-05-04 00:34:49 +0600

    chefsData send

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+const chefsData = require('./chefsData.json')
+
 app.listen(5000,() => {
     console.log('server is running on port 5000')
+})
+
+app.get('/chefsData',(req,res)=>{
+    res.send(chefsData)
 })
 
 app.get('/',(req,res) => {
