8fe22faced1882458887e6d1a089da85f60a3275
AhmedRahi18 <71ahmedrahi@gmail.com>
2023-05-04 15:07:07 +0600

    some id added

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cors = require('cors')
 app.use(cors())
 
 const chefsData = require('./chefsData.json')
-
+// server port
 app.listen(5000,() => {
-    console.log('server is running on port 5000')
+    console.log('Server is running on port 5000')
 })
-
+// chefsData
 app.get('/chefsData',(req,res)=>{
     res.send(chefsData)
 })
-
+// chefsData with id
 app.get('/chefsData/:id',(req,res)=>{
     const id = req.params.id
     const selectedChefsData = chefsData.find(c=>c.id === id)
